3c95eb87c5fde4ce6cc7360b2855bc84094a8892
mills <milindvishnoi@gmail.com>
2020-05-30 22:58:12 +0530

    vue-resource

---
+++ vue-resource/backend/flaskr/app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, request, abort, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+
+from models import setup_db, Question, Category, db
+
+QUESTIONS_PER_PAGE = 10
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, resources={r"*": {"origins": "*"}})
+
+    return app
---
+++ vue-resource/frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input class="form-control" type="text" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label>Mail</label>
+                    <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: ''
+                }
+            };
+        },
+        methods: {
+            submit() {
+                this.$http.post("http://localhost:5000/login", this.user)
+                    .then(response => () {
+                        // do something
+                });
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
